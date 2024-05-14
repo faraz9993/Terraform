c943c96cd46998920c223b4b8f88ff6e4fc08655
faraz9993 <147621846+faraz9993@users.noreply.github.com>
2024-05-14 12:04:25 +0530

    Deleting Terraform_Task

--- Terraform_Task/main.tf
+++
@@ -1,7 +0,0 @@
-module "IAM" {
-    source = "./modules/IAM/"
-}
-
-module "S3" {
-    source = "./modules/S3/"
-}
--- Terraform_Task/modules/S3/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "first_resource" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name        = "first"
-  }
-}
